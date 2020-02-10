35ec178a2c7272004b956f3bd0fa2701f08ce07c
fvanroie <fvanroie@users.noreply.github.com>
2020-02-10 22:06:35 +0100

    Update defines

--- include/hasp_conf.h
+++ include/hasp_conf.h
@@ -1,13 +1,21 @@
 #pragma once
 
-#define LV_USE_HASP 1
+#define USE_HASP_APP 1
 
-#define LV_USE_HASP_WIFI 1
-#define LV_USE_HASP_MQTT 1
-#define LV_USE_HASP_HTTP 1
-#define LV_USE_HASP_SPIFFS 1
+#define HASP_USE_WIFI 1
+#define HASP_USE_MQTT 1
+#define HASP_USE_HTTP 1
+#define HASP_USE_MDNS 0
+#define HASP_USE_SYSLOG 0
 
-#define LV_DEMO_WALLPAPER 0 /*Create a wallpaper too*/
+#define HASP_USE_SPIFFS 1
+#define HASP_USE_EEPROM 0
+#define HASP_USE_SDCARD 0
+
+#define HASP_USE_QRCODE 1
+#define HASP_USE_PNGDECODE 0
+
+//#define LV_DEMO_WALLPAPER 0 /*Create a wallpaper too*/
 
 //#define LV_HASP_HOR_RES_MAX 128
 //#define LV_HASP_VER_RES_MAX 160
