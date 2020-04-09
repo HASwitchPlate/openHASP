7031b6740614b263155fc8793a864d2943550e9c
fvanroie <fvanroie@users.noreply.github.com>
2020-04-10 00:11:54 +0200

    Add backlight GPIOs

--- src/hasp_http.cpp
+++ src/hasp_http.cpp
@@ -1385,10 +1385,12 @@ void webHandleGuiConfig()
     httpMessage += F("<p><b>Backlight Control</b> <select id='bcklpin' name='bcklpin'>");
     httpMessage += getOption(-1, F("None"), bcklpin == -1);
 #if defined(ARDUINO_ARCH_ESP32)
-    httpMessage += getOption(16, F("GPIO 16"), bcklpin == 16);
-    httpMessage += getOption(17, F("GPIO 17"), bcklpin == 17);
+    httpMessage += getOption(5, F("GPIO 5"), bcklpin == 5);
+    httpMessage += getOption(18, F("GPIO 18"), bcklpin == 18);
+    httpMessage += getOption(19, F("GPIO 19"), bcklpin == 19);
     httpMessage += getOption(21, F("GPIO 21"), bcklpin == 21);
     httpMessage += getOption(22, F("GPIO 22"), bcklpin == 22);
+    httpMessage += getOption(23, F("GPIO 23"), bcklpin == 23);
 #else
     httpMessage += getOption(5, F("D1 - GPIO 5"), bcklpin == 5);
     httpMessage += getOption(4, F("D2 - GPIO 4"), bcklpin == 4);
