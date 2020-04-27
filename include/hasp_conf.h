8531b03b048aba4511ea008284d4a5938ecac36d
fvanroie <cpt_jack@msn.com>
2020-04-28 00:08:10 +0200

    Updates for stmf407vg

--- include/hasp_conf.h
+++ include/hasp_conf.h
@@ -7,16 +7,22 @@
 
 #define HASP_USE_APP 1
 
-#define HASP_USE_OTA 1
-#define HASP_USE_WIFI 1
-#define HASP_USE_MQTT 1
-#define HASP_USE_HTTP 1
-#define HASP_USE_MDNS 1
-#define HASP_USE_SYSLOG 1
-#define HASP_USE_TELNET 1
+/* Network */
+#define HASP_HAS_NETWORK (ARDUINO_ARCH_ESP32>0 || ARDUINO_ARCH_ESP8266>0)
 
-#define HASP_USE_SPIFFS 1
-#define HASP_USE_EEPROM 0
+#define HASP_USE_OTA (HASP_HAS_NETWORK)
+#define HASP_USE_WIFI (HASP_HAS_NETWORK)
+#define HASP_USE_MQTT (HASP_HAS_NETWORK)
+#define HASP_USE_HTTP (HASP_HAS_NETWORK)
+#define HASP_USE_MDNS (HASP_HAS_NETWORK)
+#define HASP_USE_SYSLOG (HASP_HAS_NETWORK)
+#define HASP_USE_TELNET (HASP_HAS_NETWORK)
+
+/* Filesystem */
+#define HASP_HAS_FILESYSTEM (ARDUINO_ARCH_ESP32>0 || ARDUINO_ARCH_ESP8266>0)
+
+#define HASP_USE_SPIFFS (HASP_HAS_FILESYSTEM)
+#define HASP_USE_EEPROM (HASP_HAS_FILESYSTEM)
 #define HASP_USE_SDCARD 0
 
 #define HASP_USE_BUTTON 1
@@ -33,4 +39,50 @@
 #define HASP_NUM_PAGES 4
 #endif
 
+
+#if HASP_USE_SPIFFS>0
+#if defined(ARDUINO_ARCH_ESP32)
+#include "SPIFFS.h"
+#include "hasp_eeprom.h"
+#endif
+#include <FS.h> // Include the SPIFFS library
+#include "hasp_spiffs.h"
+#endif
+
+#if HASP_USE_EEPROM>0
+#include "hasp_eeprom.h"
+#endif
+
+#if HASP_USE_WIFI>0
+#include "hasp_wifi.h"
+#endif
+
+#if HASP_USE_MQTT>0
+#include "hasp_mqtt.h"
+#endif
+
+#if HASP_USE_HTTP>0
+#include "hasp_http.h"
+#endif
+
+#if HASP_USE_TELNET>0
+#include "hasp_telnet.h"
+#endif
+
+#if HASP_USE_MDNS>0
+#include "hasp_mdns.h"
+#endif
+
+#if HASP_USE_BUTTON>0
+#include "hasp_button.h"
+#endif
+
+#if HASP_USE_OTA>0
+#include "hasp_ota.h"
+#endif
+
+#endif
+
+#ifndef FPSTR
+#define FPSTR(pstr_pointer) (reinterpret_cast<const __FlashStringHelper *>(pstr_pointer))
 #endif
