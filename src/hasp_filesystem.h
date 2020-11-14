1bedc9da028b5833d73181784093f088c78b6b91
fvanroie <cpt_jack@msn.com>
2020-11-14 15:29:06 +0100

    Prepare for LittleFS

--- src/hasp_filesystem.h
+++ src/hasp_filesystem.h
@@ -8,23 +8,25 @@ bool filesystemSetup(void);
 void filesystemList();
 void filesystemInfo();
 
-
 #if defined(ARDUINO_ARCH_ESP32)
+#if HASP_USE_SPIFFS > 0
+#include "SPIFFS.h"
+#elif HASP_USE_LITTLEFS > 0
+#include "LITTLEFS.h"
+#endif
+#elif defined(ARDUINO_ARCH_ESP8266)
+// included by default
+#endif // ARDUINO_ARCH
+
+#if defined(ARDUINO_ARCH_ESP8266) || defined(ARDUINO_ARCH_ESP32)
 #include <FS.h>
 #include <ESP.h>
 
 #if HASP_USE_SPIFFS > 0
-#include "SPIFFS.h"
-extern FS * HASP_FS = &SPIFFS;
-#elif HASP_USE_LITTLEFS > 0
-#include "LittleFS.h"
-#define HASP_FS LITTLEFS
-#endif
-
-#elif defined(ARDUINO_ARCH_ESP8266)
-#include <FS.h>
-#include <ESP.h>
 #define HASP_FS SPIFFS
-#endif
+#elif HASP_USE_LITTLEFS > 0
+#define HASP_FS LITTLEFS
+#endif // HASP_USE
+#endif // ARDUINO_ARCH
 
-#endif
+#endif // HASP_FILESYSTEM_H
