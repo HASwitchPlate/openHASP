8d3fec7da9d0b27cab43c50cdd35c5e3727271b9
fvanroie <15969459+fvanroie@users.noreply.github.com>
2021-02-16 17:08:52 +0100

    Move files around

---
+++ src/hal/hasp_hal.h
@@ -0,0 +1,21 @@
+/* MIT License - Copyright (c) 2020 Francis Van Roie
+   For full license information read the LICENSE file in the project folder */
+
+#ifndef HASP_HAL_H
+#define HASP_HAL_H
+
+#include <Arduino.h>
+
+void halRestartMcu(void);
+uint8_t halGetHeapFragmentation(void);
+String halGetResetInfo(void);
+size_t halGetMaxFreeBlock(void);
+size_t halGetFreeHeap(void);
+String halGetCoreVersion(void);
+String halGetChipModel();
+String halGetMacAddress(int start, const char * seperator);
+uint16_t halGetCpuFreqMHz(void);
+String halDisplayDriverName(void);
+String halGpioName(uint8_t gpio);
+
+#endif
