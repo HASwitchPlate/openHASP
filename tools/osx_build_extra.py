4ff22c622ecde9624f32a9ff619b6c7f9a82cbe8
Diogo Gomes <dgomes@ua.pt>
2021-03-06 11:43:04 +0000

    remove print

--- tools/osx_build_extra.py
+++ tools/osx_build_extra.py
@@ -12,4 +12,4 @@ env.Replace(CC="gcc-10", CXX="g++-10")
 env.Replace(BUILD_SCRIPT="tools/osx_build_script.py")
 
 #print('=====================================')
-print(env.Dump())
+#print(env.Dump())
