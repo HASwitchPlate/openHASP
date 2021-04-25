562d7236ed57f59f3d25d36730cf098e9924931c
fvanroie <15969459+fvanroie@users.noreply.github.com>
2021-04-25 22:27:54 +0200

    Fix get_model implementation

--- src/dev/device.cpp
+++ src/dev/device.cpp
@@ -1,2 +1,19 @@
 /* MIT License - Copyright (c) 2019-2021 Francis Van Roie
    For full license information read the LICENSE file in the project folder */
+
+#include "device.h"
+
+#define Q(x) #x
+#define QUOTE(x) Q(x)
+
+namespace dev {
+
+const char* BaseDevice::get_model()
+{
+#ifdef HASP_MODEL
+    return QUOTE(HASP_MODEL);
+#else
+    return PIOENV;
+#endif
+}
+}
