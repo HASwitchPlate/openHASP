2bd650ec4f6c857d82bab3855a978523be2ea3e3
fvanroie <15969459+fvanroie@users.noreply.github.com>
2021-05-11 20:46:57 +0200

    Create regional files for Portuguese

---
+++ src/lang/pt_BR.h
@@ -0,0 +1,16 @@
+#ifndef HASP_LANG_PT_BR_H
+#define HASP_LANG_PT_BR_H
+
+#include "pt_PT.h" // Common language file
+
+// Overrides
+#undef D_USERNAME
+#define D_USERNAME "Usuário:"
+
+#undef D_PASSWORD
+#define D_PASSWORD "Senha:"
+
+#undef D_NETWORK_CONNECTION_FAILED
+#define D_NETWORK_CONNECTION_FAILED "Falhou a conexão"
+
+#endif
