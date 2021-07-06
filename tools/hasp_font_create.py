c83afd4f341ec5740cd3a3f6e5f814edf884a522
fvanroie <15969459+fvanroie@users.noreply.github.com>
2021-07-06 04:12:07 +0200

    Update hasp_font_create.py

--- tools/hasp_font_create.py
+++ tools/hasp_font_create.py
@@ -15,20 +15,29 @@ from jsmin import jsmin
 with open("src/font/encodings.json") as js_file:
     minified = jsmin(js_file.read())
     fonts = json.loads(minified)
-print(fonts)
+#print(fonts)
 
 with open("src/font/md-icons.json") as js_file:
     minified = jsmin(js_file.read())
     icons = json.loads(minified)
-print(icons)
+#print(icons)
 
 symbol_list = []
-for (code, desc) in icons["icons"].items():
-    if code != "0":
-        symbol_list.append('"{}"'.format(str(code)))
+symbol_names = []
+for (obj,list) in icons["include"].items():
+    for name in list:
+        if name != "0":
+            # print(name)
+            code = icons["icons"][name]
+            symbol_list.append('"{}"'.format(str(code)))
+            symbol_names.append('"{}"'.format(str(name)))
 
+symbol_list.sort()
+symbol_names.sort()
 symbols = ",".join(symbol_list)
-print(symbols)
+
+print(",".join(symbol_names))
+print("{} icons selected".format(len(symbol_list)))
 
 compr = "--no-kerning"
 bpp = 3
