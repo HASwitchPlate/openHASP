2d3e498a89eda5ecabf8768ef80656c226f38a9a
fvanroie <15969459+fvanroie@users.noreply.github.com>
2021-11-22 11:20:04 +0100

    Add GPIO0 to possible backlight pins

--- src/sys/svc/hasp_http.cpp
+++ src/sys/svc/hasp_http.cpp
@@ -1150,10 +1150,12 @@ static void webHandleGuiConfig()
         httpMessage += getOption(-1, F("None"), bcklpin);
 #if defined(ARDUINO_ARCH_ESP32)
         char buffer[10];
-        uint8_t pins[] = {5, 12, 13, 15, 16, 17, 18, 19, 21, 22, 23, 32};
+        uint8_t pins[] = {0, 5, 12, 13, 15, 16, 17, 18, 19, 21, 22, 23, 32};
         for(uint8_t i = 0; i < sizeof(pins); i++) {
-            snprintf_P(buffer, sizeof(buffer), PSTR("GPIO %d"), pins[i]);
-            httpMessage += getOption(pins[i], buffer, bcklpin);
+            // if(!gpioIsSystemPin(pins[i])) {
+                snprintf_P(buffer, sizeof(buffer), PSTR("GPIO %d"), pins[i]);
+                httpMessage += getOption(pins[i], buffer, bcklpin);
+            // }
         }
 #else
         httpMessage += getOption(5, F("D1 - GPIO 5"), bcklpin);
