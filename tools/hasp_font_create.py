bd256bddd8986f39c43aa3af55d383cabca1df3e
fvanroie <15969459+fvanroie@users.noreply.github.com>
2022-02-12 10:22:27 +0100

    Add chinese characters to encoding.json #249

--- tools/hasp_font_create.py
+++ tools/hasp_font_create.py
@@ -22,26 +22,25 @@ with open("src/font/md-icons.json") as js_file:
     icons = json.loads(minified)
 #print(icons)
 
-symbol_list = []
-symbol_names = []
+icon_list = []
+icon_names = []
 for (obj,list) in icons["include"].items():
     for name in list:
         if name != "0":
             # print(name)
             code = icons["icons"][name]
-            symbol_list.append('"{}"'.format(str(code)))
-            symbol_names.append('"{}"'.format(str(name)))
+            icon_list.append('"{}"'.format(str(code)))
+            icon_names.append('"{}"'.format(str(name)))
 
-symbol_list.sort()
-symbol_names.sort()
-symbols = ",".join(symbol_list)
+icon_list.sort()
+icon_names.sort()
+icon_symbols = ",".join(icon_list)
 
-print(",".join(symbol_names))
-print("{} icons selected".format(len(symbol_list)))
+print(",".join(icon_names))
+print("{} icons selected".format(len(icon_list)))
 
 compr = "--no-kerning"
 bpp = 3
-size = 16
 shift = int("0xE2000", base=0)
 
 for (item, data) in fonts.items():
@@ -50,15 +49,25 @@ for (item, data) in fonts.items():
         chars = []
         for (desc, char) in data["chars"].items():
             chars.append(char)
+        symbols = []
+        for (desc, symbol) in data["symbols"].items():
+            symbols.append(symbol)
 
-        cmd = "lv_font_conv {} --bpp {} --size {} --font {} -r {} --font {} -r {} --format bin -o {} --format lvgl".format(
+        if len(symbols)>0:
+            symbol_flag = "--symbols"
+        else:
+            symbol_flag = ""
+
+        cmd = "lv_font_conv {} --bpp {} --size {} --font {} -r {} {} {} --font {} -r {} -o {} --format lvgl".format(
             compr,
             bpp,
             size,
             data["textfont"],
             ",".join(chars),
+            symbol_flag,
+            "".join(symbols),
             icons["iconfont"],
-            symbols,
+            icon_symbols,
             output,
         )
         os.system(cmd)
