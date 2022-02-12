2f5a1ff4dc2129f6c6ca99439d90a9b9830e088b
fvanroie <15969459+fvanroie@users.noreply.github.com>
2022-02-12 16:35:56 +0100

    Fixed a typo

--- src/drv/touch/touch_driver.h
+++ src/drv/touch/touch_driver.h
@@ -48,7 +48,7 @@ class BaseTouch {
 #define TOUCH_DRIVER -1 // No Touch
 #endif
 
-if TOUCH_DRIVER == 0x2046 && defined(USER_SETUP_LOADED)
+#if TOUCH_DRIVER == 0x2046 && defined(USER_SETUP_LOADED)
 #warning Building for XPT2046
 //#include "touch_driver_xpt2046.h"
 #include "touch_driver_tftespi.h"
