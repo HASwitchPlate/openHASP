ed40f28b7a78b256d39ea5e8702919ef64ffe607
fvanroie <cpt_jack@msn.com>
2022-03-02 18:18:55 +0100

    Fix bootloader path for ESP32 and ESP32S2 #307

--- tools/esp_merge_bin.py
+++ tools/esp_merge_bin.py
@@ -34,6 +34,8 @@ def copy_merge_bins(source, target, env):
     flash_size = env.GetProjectOption("board_upload.flash_size")
 
     bootloader = "{}tools{}sdk{}esp32{}bin{}bootloader_dio_40m.bin".format(FRAMEWORK_DIR, os.path.sep, os.path.sep, os.path.sep, os.path.sep, os.path.sep)
+    if not os.path.isfile(bootloader):
+        bootloader = "{}tools{}sdk{}bin{}bootloader_dio_40m.bin".format(FRAMEWORK_DIR, os.path.sep, os.path.sep, os.path.sep, os.path.sep, os.path.sep)
     partitions = "{}{}partitions.bin".format(env.subst("$BUILD_DIR"), os.path.sep)
     boot_app0 = "{}tools{}partitions{}boot_app0.bin".format(FRAMEWORK_DIR, os.path.sep, os.path.sep, os.path.sep)
     firmware_dst ="{}firmware{}{}_full_{}_{}.bin".format(OUTPUT_DIR, os.path.sep, name, flash_size, version)
