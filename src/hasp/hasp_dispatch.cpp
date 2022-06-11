94f36ba1b6dc3faca7e28470b57be1771eff52ce
fvanroie <cpt_jack@msn.com>
2022-06-11 21:08:21 +0200

    Fix NetworkStop for native builds

--- src/hasp/hasp_dispatch.cpp
+++ src/hasp/hasp_dispatch.cpp
@@ -1040,7 +1040,9 @@ void dispatch_reboot(bool saveConfig)
 #if HASP_USE_CONFIG > 0
     debugStop();
 #endif
+#if HASP_USE_WIFI > 0 || HASP_USE_ETHERNET > 0
     networkStop();
+#endif
     LOG_VERBOSE(TAG_MSGR, F("-------------------------------------"));
     LOG_TRACE(TAG_MSGR, F(D_DISPATCH_REBOOT));
 
