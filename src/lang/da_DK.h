06131f4171c9773f9bb67ea01e329e7f52d1f189
fvanroie <15969459+fvanroie@users.noreply.github.com>
2023-08-22 17:59:22 +0200

    New translations en_US.h (Danish)

--- src/lang/da_DK.h
+++ src/lang/da_DK.h
@@ -1,3 +1,6 @@
+// This translation file is maintained on https://crowdin.com/project/openhasp/
+// Do not edit directly!
+
 #ifndef HASP_LANG_EN_US_H
 #define HASP_LANG_EN_US_H
 
@@ -209,7 +212,6 @@
 #define D_WIFI_RSSI_WEAK "Weak"
 #define D_WIFI_RSSI_BAD "Very bad"
 
-// new
 #define D_GPIO_SWITCH "Switch"
 #define D_GPIO_BUTTON "Push Button"
 #define D_GPIO_TOUCH "Capacitive Touch"
