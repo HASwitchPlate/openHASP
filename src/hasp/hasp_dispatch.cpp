e092b72a6b296167777b4762b458552bcd9c1b5c
Jaroslav Kysela <perex@perex.cz>
2023-11-29 18:10:27 +0100

    always run idle scripts when sleep state changes

--- src/hasp/hasp_dispatch.cpp
+++ src/hasp/hasp_dispatch.cpp
@@ -1356,14 +1356,11 @@ void dispatch_idle(const char*, const char* payload, uint8_t source)
     if(payload && strlen(payload)) {
         uint8_t state = HASP_SLEEP_LAST;
         if(!strcmp_P(payload, "off")) {
-            hasp_set_sleep_state(HASP_SLEEP_OFF);
-            dispatch_run_script(NULL, "L:/idle_off.cmd", source);
+            hasp_set_sleep_state(HASP_SLEEP_OFF, source);
         } else if(!strcmp_P(payload, "short")) {
-            hasp_set_sleep_state(HASP_SLEEP_SHORT);
-            dispatch_run_script(NULL, "L:/idle_short.cmd", source);
+            hasp_set_sleep_state(HASP_SLEEP_SHORT, source);
         } else if(!strcmp_P(payload, "long")) {
-            hasp_set_sleep_state(HASP_SLEEP_LONG);
-            dispatch_run_script(NULL, "L:/idle_long.cmd", source);
+            hasp_set_sleep_state(HASP_SLEEP_LONG, source);
         } else {
             LOG_WARNING(TAG_MSGR, F("Invalid idle value %s"), payload);
             return;
