e756b4b4e2877a974dcf4900139415da8ccaa326
Alvaro Lobato <alvarolobato@gmail.com>
2023-12-22 00:02:05 +0100

    Fix mac build: - Change compiler version so it works with newer MacOS - Fix library paths - Exclude freetype which creates a build error

--- tools/osx_build_extra.py
+++ tools/osx_build_extra.py
@@ -1,6 +1,7 @@
 Import("env")
 
-env.Replace(CC="gcc-10", CXX="g++-10")
+#env.Replace(CC="gcc-10", CXX="g++-10")
+env.Replace(CC="gcc-12", CXX="g++-12")
 
 env.Replace(BUILD_SCRIPT="tools/osx_build_script.py")
 
