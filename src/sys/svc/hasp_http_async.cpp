0f64a3b0f457414a534e62855885af05b35d0c05
Shubham Sharma <10323297+shubham-vunet@users.noreply.github.com>
2024-10-02 21:17:45 +0530

    Add "Lilygo T-Panel v1.2" pinmap and touch screen

--- src/sys/svc/hasp_http_async.cpp
+++ src/sys/svc/hasp_http_async.cpp
@@ -1311,7 +1311,7 @@ void webHandleGuiConfig(AsyncWebServerRequest* request)
         httpMessage += getOption(-1, F("None"), bcklpin == -1);
 #if defined(ARDUINO_ARCH_ESP32)
         add_gpio_select_option(httpMessage, 5, bcklpin);  // D8 on ESP32 for D1 mini 32
-        add_gpio_select_option(httpMessage, 12, bcklpin); // TFT_LED on the Liligo Pi
+        add_gpio_select_option(httpMessage, 12, bcklpin); // TFT_LED on the Lilygo Pi
         add_gpio_select_option(httpMessage, 13, bcklpin); // TFT_LED on the D1 R32 + Waveshare
         add_gpio_select_option(httpMessage, 15, bcklpin); // TFT_LED on the AZ Touch
         add_gpio_select_option(httpMessage, 16, bcklpin); // D4 on ESP32 for D1 mini 32
