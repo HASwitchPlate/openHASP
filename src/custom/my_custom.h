817bf98196bf67901c623fc2accff41379a2b44d
vorbeiei <vorbeiei@users.noreply.github.com>
2024-12-15 14:53:08 +0100

    Update my_custom.h
    ci skip

--- src/custom/my_custom.h
+++ src/custom/my_custom.h
@@ -9,7 +9,7 @@
 #define HASP_CUSTOM_H
 
 #include "hasplib.h"
-#if defined(HASP_USE_CUSTOM)
+#if defined(HASP_USE_CUSTOM) && HASP_USE_CUSTOM > 0
 
 //extern float batteryFraction; //shared with hasp_gui.cpp
 
@@ -19,9 +19,18 @@ void custom_setup();
 /* This function is called every itteration of the main loop */
 void custom_loop();
 
+void custom_every_second();
+
+/* This function is called every 5 seconds */
+void custom_every_5seconds();
+
+/* return true if the pin used by the custom code */
+bool custom_pin_in_use(uint8_t pin);
+
 /* Add a key which defines a JsonObject to add to the sensor JSON output */
 void custom_get_sensors(JsonDocument& doc);
 
+void custom_state_subtopic(const char* subtopic, const char* payload);
 /* Receive custom topic & payload messages */
 void custom_topic_payload(const char* topic, const char* payload, uint8_t source);
 
