2fc239482b9e2a1db2331b6394e5009319674479
vorbeiei <vorbeiei@users.noreply.github.com>
2024-12-15 12:49:00 +0100

    add custom code for battery voltage
    from https://github.com/elik745i/ESP32-2432s024c

---
+++ src/custom/my_custom.cpp
@@ -0,0 +1,111 @@
+/* MIT License - Copyright (c) 2019-2022 Francis Van Roie
+   For full license information read the LICENSE file in the project folder */
+
+// USAGE: - Copy this file and rename it to my_custom.cpp
+//        - Change false to true on line 9
+//280mA - LED ON, 190mA - LED off
+
+#include "hasplib.h"
+//#include <lvgl.h>
+
+#if defined(HASP_USE_CUSTOM) && true // <-- set this to true in your code
+
+#include "hasp_debug.h"
+#include "custom/my_custom.h"
+
+
+const int voltage_read = 35;
+const int blink_speed = 60000; //read every 60 sec
+
+float batteryFraction;
+float currentVoltage;
+
+//extern lv_obj_t* battery_bar;  // Declare the battery bar variable
+//lv_obj_t* battery_bar;  // Declare the battery bar variable
+
+// Implement heartbeat led
+unsigned long lastMillis = 0;
+
+//Voltage read
+const int MAX_ANALOG_VAL = 4095;
+const float MAX_BATTERY_VOLTAGE = 4.2; // Max LiPoly voltage of a 3.7 battery is 4.2
+const float minVoltage = 3.0;  // Minimum voltage (0% charge)
+const float maxVoltage = 4.2;  // Maximum voltage (100% charge)
+
+//deep sleep timer
+const int sleepTimeSeconds = 60;  // Set the sleep time in seconds
+
+void custom_setup()
+{
+    // Initialization code here
+    last_blink = millis();
+    pinMode(voltage_read, INPUT_PULLUP);
+
+    randomSeed(millis());
+}
+
+void custom_loop()
+{
+    // read voltage every 60 seconds
+    if(blink_speed && (millis() - last_blink > blink_speed)) {
+
+        currentVoltage = analogRead(voltage_read) * (maxVoltage / 4095.0);
+        // Calculate the percentage of charge
+        batteryFraction = map(constrain(currentVoltage, minVoltage, maxVoltage) * 1000, minVoltage * 1000, maxVoltage * 1000, 0, 100);
+        //read illumination
+
+        last_blink = millis();
+        
+        updateBatteryDisplay(12, 9, batteryFraction);
+        updateBatteryDisplay(9, 9, batteryFraction);
+        updateBatteryDisplay(0, 7, batteryFraction);
+        //updateVoltageDisplay(9,10,currentVoltage);
+        String voltageString = String(currentVoltage, 2);     // Converts the float to a String with 2 decimal places
+        voltageString += "V";                                 // Concatenates "V" at the end
+        updateTextDisplay(12, 10, voltageString.c_str());
+        String fractionString = String(batteryFraction, 2);   // Converts the float to a String with 2 decimal places
+        fractionString += "%";                                // Concatenates "%" at the end 
+        updateTextDisplay(9, 11, fractionString.c_str());  
+        updateTextDisplay(12, 11, fractionString.c_str());
+    }
+
+}
+
+//search bar element and apply adata float value to it
+void updateBatteryDisplay(uint8_t page, uint8_t id, float adata) {
+    lv_obj_t* widget = hasp_find_obj_from_page_id(page, id);
+    if (!widget) return; // object doesn't exist
+
+    // Calculate the color based on battery percentage
+    uint8_t red = constrain(map(adata, 0, 100, 255, 0), 0, 255);
+    uint8_t green = constrain(map(adata, 0, 100, 0, 255), 0, 255);
+    uint8_t blue = 0; // Assuming no blue component
+
+    // Set the calculated color
+    lv_color_t color = LV_COLOR_MAKE(red, green, blue);
+    lv_obj_set_style_local_bg_color(widget, LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, color);
+
+
+    lv_bar_set_value(widget, adata, LV_ANIM_OFF);
+}
+
+//search label element and apply adata text value to it
+void updateTextDisplay(uint8_t page, uint8_t id, const char* text) {
+    lv_obj_t* widget = hasp_find_obj_from_page_id(page, id);
+    if (!widget) return; // object doesn't exist
+    lv_label_set_text(widget, text);
+}
+
+
+void custom_get_sensors(JsonDocument& doc)
+{
+    JsonObject sensor = doc.createNestedObject(F("Battery"));  // Add Key
+    sensor[F("Battery")] = batteryFraction;                        // Set Value
+
+}
+
+void custom_topic_payload(const char* topic, const char* payload, uint8_t source){
+    // Not used
+}
+
+#endif // HASP_USE_CUSTOM
